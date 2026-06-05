30c74f36b4afb38e721178e198bdba1de3e8f999
wjj <17752576852@139.com>
2026-06-05 21:00:53 +0800

    fix-cardExtend

--- src/main/java/com/v1/service/impl/CardsrecordServiceImpl.java
+++ src/main/java/com/v1/service/impl/CardsrecordServiceImpl.java
@@ -3,11 +3,9 @@ package com.v1.service.impl;
 import com.v1.pojo.Cardsrecord;
 import com.v1.mapper.CardsrecordMapper;
 import com.v1.pojo.Member;
-import com.v1.pojo.Membertype;
 import com.v1.service.CardsrecordService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.v1.service.MemberService;
-import com.v1.service.MembertypeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -25,24 +23,22 @@ public class CardsrecordServiceImpl extends ServiceImpl<CardsrecordMapper, Cards
 
     @Autowired
     MemberService memberService;
-    @Autowired
-    MembertypeService membertypeService;
-    @Autowired
-    private CardsrecordService cardsrecordService;
 
     @Override
     @Transactional
     public void extendCard(Cardsrecord cardsrecord) {
         //更新到期时间和余额信息和卡类型
         Member member = memberService.getById(cardsrecord.getMemberId());
+        if (member == null) {
+            throw new RuntimeException("会员不存在，续卡失败");
+        }
         Integer typeId = cardsrecord.getTypeId();
-//        member.setMembertype(membertypeService.getById(typeId));
         member.setMemberTypes(typeId);
         member.setMemberStatic(1);
         member.setMemberxufei(cardsrecord.getDaoqi());
         member.setMemberbalance(member.getMemberbalance() - cardsrecord.getMoney());
         memberService.updateById(member);
         //添加续卡记录
-        cardsrecordService.save(cardsrecord);
+        save(cardsrecord);
     }
 }
